a444e3337df8b5b743d093ec87e2d37a3b32c25f
NoeliaAT <noelia.almaraz.et12@gmail.com>
2024-06-14 11:48:09 -0300

    falta mysql

--- Api/Persistencia/AplicacionDbContext.cs
+++ Api/Persistencia/AplicacionDbContext.cs
@@ -11,4 +11,17 @@ public class AplicacionDbContext : DbContext
     }
     public DbSet<Usuario> Usuarios { get; set; }
     public DbSet<Domicilio> Domicilios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>().HasData(
+            new Usuario ("Usuario1", "ApellidoA", "Dirección1" ),
+            new Usuario ("Usuario2", "ApellidoB", "Dirección2" )
+        );
+
+        modelBuilder.Entity<Domicilio>().HasData(
+            new Domicilio ("Domicilio1", 1, 1 ),
+            new Domicilio ("Domicilio2", 2, 2 )
+        );
+    }
 }
