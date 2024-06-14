f6c2e08a20d0465c6daeb56e6e23e2461b2d6ad8
NoeliaAT <noelia.almaraz.et12@gmail.com>
2024-06-14 09:38:54 -0300

    Problema Migraciones

--- Api/Program.cs
+++ Api/Program.cs
@@ -23,6 +23,15 @@ builder.Services.AddDbContext<AplicacionDbContext>(opcion =>
 builder.Services.AddDbContext<AplicacionDbContext>();
 
 
+var opciones = new DbContextOptionsBuilder<AplicacionDbContext>();
+
+opciones.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 30)));
+
+var contexto = new AplicacionDbContext(opciones.Options);
+
+contexto.Database.EnsureCreated();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -47,4 +56,7 @@ app.MapControllers();
 app.Run();
 
 
-// video 4 minuto 18:50//
+// video 4 minuto 29:00//
+// revisar msql en casa, revisar version,  video 4 minuto 24:00 //
+//code farce:  es código fuente, primero hago el código c# y después de ahi genera el sql
+//daba base farce: a partir de una base de datos existente (con datos o no cargados), tiro el comando de linea para traerme la base de datos y automáticamente (Entity) lo convierte en c#
